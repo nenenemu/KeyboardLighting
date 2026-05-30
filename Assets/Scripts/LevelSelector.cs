f05bdebcb281accc17dbd5d8baad71e3b2c72b5e
nenenemu <sorabou.0320@gmail.com>
2026-05-31 00:30:09 +0900

    ittan commit
    とりまイベントトリガーのやつ消すからそれのhistory用のコミット

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,44 +1,53 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using System.Collections;
 
 public class LevelSelector : MonoBehaviour
 {
-    public static int currentLevel = 1;
+    public static GameObject lastSelectedButton = null;
+    public GameObject defaultButton; // ← Level1 のボタンを入れる
 
-    public static bool Lv1 = false;
-    public static bool Lv2 = false;
-    public static bool Lv3 = false;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(SelectAfterUIReady());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SelectAfterUIReady()
     {
-        
+        // UI が揃うまで 1 フレーム待つ（これが全て）
+        yield return null;
+
+        // 一旦クリア（null 選択を防ぐ）
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // 最後に押したボタンがあればそれを選択
+        if (lastSelectedButton != null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelectedButton);
+        }
+        else
+        {
+            // 初回は defaultButton（Level1）
+            EventSystem.current.SetSelectedGameObject(defaultButton);
+        }
     }
 
-    public void Level1()
+    public void Level1(GameObject button)
     {
+        lastSelectedButton = button;
         SceneManager.LoadScene("Level1");
     }
 
-
-    public void Level2()
+    public void Level2(GameObject button)
     {
-
+        lastSelectedButton = button;
         SceneManager.LoadScene("Level2");
     }
 
-    public void Level3()
+    public void Level3(GameObject button)
     {
-    
+        lastSelectedButton = button;
         SceneManager.LoadScene("Level3");
     }
-    
-
-
 }
